60a43f0109a98270d4130f1a928f017709e763fd
messiasjs <mjls.junior.js@gmail.com>
2023-08-25 00:56:53 -0300

    creating routes

--- gerenciamento-de-pedidos/routes/api.php
+++ gerenciamento-de-pedidos/routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PedidoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +16,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+/*Route::get('/', function(){
+    return "ok";
 });
+Route::post('/pedidos', [PedidoController::class, 'store']);
+Route::get('/pedidos', [PedidoController::class, 'index']);
+Route::get('/pedidos/{pedido}', [PedidoController::class, 'show']);
+Route::put('/pedidos/{pedido}', [PedidoController::class, 'update']);
+Route::delete('/pedidos/{pedido}', [PedidoController::class, 'destroy']);*/
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+
+Route::group(['middleware' => ['auth:sanctum']], function() {
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    Route::post('/pedidos', [PedidoController::class, 'store']);
+    Route::get('/pedidos', [PedidoController::class, 'index']);
+    Route::get('/pedidos/{pedido}', [PedidoController::class, 'show']);
+    Route::put('/pedidos/{pedido}', [PedidoController::class, 'update']);
+    Route::delete('/pedidos/{pedido}', [PedidoController::class, 'destroy']);
+    Route::post('/logout', [AuthController::class, 'login']);
+});
+
